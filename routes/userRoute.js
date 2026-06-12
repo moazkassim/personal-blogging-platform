f9c2b9a836e972da9b3dc199975ba68d21dd83bc
moaz kassim <moazkassim100@gmail.com>
2026-06-13 02:36:57 +0300

    adding use-validator with joi

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/userController.js";
+import { validate } from "../middleware/validate.js";
+import { registerSchema, loginSchema } from "../validators/userValidator.js";
 const userRouter = express.Router();
-userRouter.post("/auth/register", registerUser);
-userRouter.post("/auth/login", loginUser);
+userRouter.post("/auth/register", validate(registerSchema), registerUser);
+userRouter.post("/auth/login", validate(loginSchema), loginUser);
 
 export default userRouter;
